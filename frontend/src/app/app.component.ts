abd9c397f120f578d231502321ec657759afdf23
Arishemm <arishem51@gmail.com>
2024-10-04 14:22:27 +0700

    feat: styling material & Login page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,27 +1,54 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatInputModule, MatButtonModule],
+  styles: [
+    `
+      .login-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        padding: 20px;
+        background-color: #f4f7fa;
+      }
+
+      .full-width {
+        width: 100%;
+        max-width: 300px;
+        margin-bottom: 15px;
+      }
+    `,
+  ],
   template: `
-    <div>
+    <div class="login-container">
       <h1>Login</h1>
-      <label>Username: </label>
-      <input
-        type="text"
-        [(ngModel)]="input.username"
-        (ngModelChange)="onUsernameChange($event)"
-      />
-      <br />
-      <br />
-      <label>Password: </label>
-      <input
-        type="password"
-        [(ngModel)]="input.password"
-        (ngModelChange)="onPasswordChange($event)"
-      />
+      <mat-form-field appearance="fill" class="full-width">
+        <mat-label>Username</mat-label>
+        <input
+          matInput
+          [(ngModel)]="input.username"
+          (ngModelChange)="onUsernameChange($event)"
+        />
+      </mat-form-field>
+      <mat-form-field appearance="fill" class="full-width">
+        <mat-label>Password</mat-label>
+        <input
+          matInput
+          [(ngModel)]="input.password"
+          (ngModelChange)="onPasswordChange($event)"
+          type="password"
+        />
+      </mat-form-field>
+      <button mat-raised-button color="primary" class="full-width">
+        Login
+      </button>
     </div>
   `,
 })
