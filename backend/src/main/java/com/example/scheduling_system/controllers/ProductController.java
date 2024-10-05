5ff4d127ec5cc3e1b91201c7176f3dc3d0139ec1
Arishemm <arishem51@gmail.com>
2024-10-05 20:03:25 +0700

    feat: product crud

--- backend/src/main/java/com/example/scheduling_system/controllers/ProductController.java
+++ backend/src/main/java/com/example/scheduling_system/controllers/ProductController.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,6 +46,28 @@ public class ProductController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new BodyResponse<>("Create product error!").getBodyResponse());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable String id, @RequestBody ProductRequest request) {
+        try {
+            var product = productService.update(Long.parseLong(id), request);
+            return ResponseEntity.ok().body(new BodyResponse<>("Success", product).getBodyResponse());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new BodyResponse<>("Update product error!").getBodyResponse());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable String id) {
+        try {
+            productService.delete(Long.parseLong(id));
+            return ResponseEntity.ok().body(new BodyResponse<>("Success").getBodyResponse());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new BodyResponse<>("Delete product error!").getBodyResponse());
         }
     }
 }
--- backend/src/main/java/com/example/scheduling_system/services/ProductService.java
+++ backend/src/main/java/com/example/scheduling_system/services/ProductService.java
@@ -1,8 +1,6 @@
 package com.example.scheduling_system.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +10,7 @@ import com.example.scheduling_system.payload.request.ProductRequest;
 import com.example.scheduling_system.payload.response.PaginateResponse;
 import com.example.scheduling_system.payload.response.ProductResponse;
 import com.example.scheduling_system.repositories.ProductRepository;
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,4 +39,23 @@ public class ProductService {
         productRepository.save(product);
         return mapProductToProductResponse(product);
     }
+
+    private Product getById(long id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeJsonMappingException("Not found!"));
+    }
+
+    public ProductResponse update(long id, ProductRequest request) {
+        var product = this.getById(id);
+        product.setName(request.name());
+        product.setImg(request.img());
+        product.setEstimatedEffort(request.estimatedEffort());
+        productRepository.save(product);
+        return mapProductToProductResponse(product);
+    }
+
+    public void delete(long id) {
+        var product = this.getById(id);
+        productRepository.delete(product);
+    }
+
 }
