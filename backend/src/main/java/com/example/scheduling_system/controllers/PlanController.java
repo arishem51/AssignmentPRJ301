fea9fefeded2b2fbbc7384836191abc7cf5a5ec3
Arishemm <arishem51@gmail.com>
2024-10-10 23:03:33 +0700

    feat: update PlanController

--- backend/src/main/java/com/example/scheduling_system/controllers/PlanController.java
+++ backend/src/main/java/com/example/scheduling_system/controllers/PlanController.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,18 +31,29 @@ public class PlanController {
             return ResponseEntity.ok().body(new BodyResponse<>("Success", plans).getBodyResponse());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new BodyResponse<>("Get product error!").getBodyResponse());
+                    .body(new BodyResponse<>("Get plan error!").getBodyResponse());
         }
     }
 
     @PostMapping
-    public ResponseEntity<?> createProduct(@RequestBody PlanRequest request) {
+    public ResponseEntity<?> create(@RequestBody PlanRequest request) {
         try {
             var plan = planService.create(request);
             return ResponseEntity.ok().body(new BodyResponse<>("Success", plan).getBodyResponse());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new BodyResponse<>("Create product error!").getBodyResponse());
+                    .body(new BodyResponse<>("Create plan error!").getBodyResponse());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody PlanRequest request) {
+        try {
+            var plan = planService.update(id, request);
+            return ResponseEntity.ok().body(new BodyResponse<>("Success", plan).getBodyResponse());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new BodyResponse<>("Create plan error!").getBodyResponse());
         }
     }
 }
